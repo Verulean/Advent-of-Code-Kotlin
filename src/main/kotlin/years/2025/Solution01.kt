d8ee3eeebafbf68769275a51a9408c0a3bb57972
Verulean <65474222+Verulean@users.noreply.github.com>
2025-12-01 01:18:02 -0500

    Day 1: Secret Entrance

--- src/main/kotlin/years/2025/Solution01.kt
+++ src/main/kotlin/years/2025/Solution01.kt
@@ -1,13 +1,34 @@
 package years.`2025`
 
-//import adventOfCode.*
-//import adventOfCode.util.*
-//
-//object Solution : Solution<Any>(AOC_YEAR, ) {
-//    override fun getInput(handler: InputHandler):  {
-//    }
-//
-//    override fun solve(input: ): Pair<Any?, Any?> {
-//        return Pair(null, null)
-//    }
-//}
+import adventOfCode.*
+import adventOfCode.util.*
+import kotlin.math.abs
+
+object Solution01 : Solution<List<Int>>(AOC_YEAR, 1) {
+    override fun getInput(handler: InputHandler) = handler.getInput("\n") {
+        it.replace('L', '-')
+            .replace('R', '+')
+            .toInt()
+    }
+
+    private fun Int.floorMod(other: Int) = (this % other + other) % other
+
+    override fun solve(input: List<Int>): PairOf<Int> {
+        var ans1 = 0
+        var ans2 = 0
+        input.fold(50) { position, offset ->
+            val rotated = position + offset
+            val newPosition = rotated.floorMod(100)
+
+            // Part 1
+            if (newPosition == 0) ans1++
+            // Part 2
+            var rotations = abs(rotated / 100)
+            if (offset < 0 && position != 0 && rotated <= 0) rotations++
+            ans2 += rotations
+
+            newPosition
+        }
+        return ans1 to ans2
+    }
+}
