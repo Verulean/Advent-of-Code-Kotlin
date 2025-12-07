c4ac362388313aa188e7e22bcb0bef439df95557
Verulean <65474222+Verulean@users.noreply.github.com>
2025-12-07 12:23:17 -0500

    Improve Day 7 performance
    Skip building splitter sets from input, and just build the result while looping through the string

--- src/main/kotlin/years/2025/Solution07.kt
+++ src/main/kotlin/years/2025/Solution07.kt
@@ -3,33 +3,27 @@ package years.`2025`
 import adventOfCode.InputHandler
 import adventOfCode.Solution
 
-object Solution07 : Solution<Pair<Int, Map<Int, Set<Int>>>>(AOC_YEAR, 7) {
-    override fun getInput(handler: InputHandler): Pair<Int, Map<Int, Set<Int>>> {
-        val data = handler.getInput("\n")
-        val startIndex = data.first().indexOf('S')
-        val splitterMap = mutableMapOf<Int, MutableSet<Int>>()
-        data.withIndex().forEach { (i, row) ->
-            row.withIndex().forEach { (j, c) ->
-                if (c == '^') splitterMap.getOrPut(i) { mutableSetOf() }.add(j)
-            }
-        }
-        return startIndex to splitterMap
-    }
+object Solution07 : Solution<List<String>>(AOC_YEAR, 7) {
+    override fun getInput(handler: InputHandler) = handler.getInput("\n")
 
     private fun MutableMap<Int, Long>.addBeams(j: Int, count: Long) = this.merge(j, count, Long::plus)
 
-    override fun solve(input: Pair<Int, Map<Int, Set<Int>>>): Pair<Int, Long> {
-        val (startIndex, splitterMap) = input
+    override fun solve(input: List<String>): Pair<Int, Long> {
         var ans1 = 0
-        val beams = mutableMapOf(startIndex to 1L)
-        splitterMap.entries.sortedBy { it.key }.forEach { (_, splitters) ->
-            splitters.forEach { j ->
-                val count = beams.getOrDefault(j, 0)
-                if (count > 0) {
-                    beams.addBeams(j - 1, count)
-                    beams.addBeams(j + 1, count)
-                    beams[j] = 0
-                    ans1++
+        val beams: MutableMap<Int, Long> = mutableMapOf()
+        input.forEach { row ->
+            row.withIndex().forEach { (j, c) ->
+                when (c) {
+                    'S' -> beams[j] = 1
+                    '^' -> {
+                        val count = beams.getOrDefault(j, 0)
+                        if (count > 0) {
+                            beams.addBeams(j - 1, count)
+                            beams.addBeams(j + 1, count)
+                            beams[j] = 0
+                            ans1++
+                        }
+                    }
                 }
             }
         }
