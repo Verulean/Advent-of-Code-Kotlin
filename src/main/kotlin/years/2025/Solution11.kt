6c3685c374d7839c1cf7f2e343ce6e51650f1b47
Verulean <65474222+Verulean@users.noreply.github.com>
2025-12-11 00:52:07 -0500

    Generalize Day 11 path counting
    Memoize counts from a given node to a defined endpoint so parts 1 and 2 can share the same code

--- src/main/kotlin/years/2025/Solution11.kt
+++ src/main/kotlin/years/2025/Solution11.kt
@@ -2,6 +2,7 @@ package years.`2025`
 
 import adventOfCode.InputHandler
 import adventOfCode.Solution
+import adventOfCode.util.PairOf
 
 private typealias ReactorGraph = Map<String, List<String>>
 
@@ -11,26 +12,21 @@ object Solution11 : Solution<ReactorGraph>(AOC_YEAR, 11) {
         source to destinations.split(" ")
     }
 
-    private val p1Memo: MutableMap<String, Int> = mutableMapOf()
-    private fun ReactorGraph.countPaths(node: String): Int = p1Memo.getOrPut(node) {
+    private val pathMemo: MutableMap<Pair<String, String>, Long> = mutableMapOf()
+    private fun ReactorGraph.countPaths(node: String, end: String): Long = pathMemo.getOrPut(node to end) {
         when (node) {
-            "out" -> 1
-            else -> this[node]?.sumOf { this.countPaths(it) } ?: 0
+            end -> 1
+            else -> this[node]?.sumOf { this.countPaths(it, end) } ?: 0
         }
     }
 
-    private val p2Memo: MutableMap<Triple<String, Boolean, Boolean>, Long> = mutableMapOf()
-    private fun ReactorGraph.countPaths(node: String, dac: Boolean, fft: Boolean): Long =
-        p2Memo.getOrPut(Triple(node, dac, fft)) {
-            when (node) {
-                "out" -> if (dac && fft) 1 else 0
-                "dac" -> this[node]?.sumOf { this.countPaths(it, true, fft) } ?: 0
-                "fft" -> this[node]?.sumOf { this.countPaths(it, dac, true) } ?: 0
-                else -> this[node]?.sumOf { this.countPaths(it, dac, fft) } ?: 0
-            }
-        }
+    private fun ReactorGraph.countPaths(steps: Collection<String>) =
+        steps.zipWithNext { node, end -> this.countPaths(node, end) }.reduce(Long::times)
 
-    override fun solve(input: ReactorGraph): Pair<Int, Long> {
-        return input.countPaths("you") to input.countPaths("svr", dac = false, fft = false)
+    override fun solve(input: ReactorGraph): PairOf<Long> {
+        val ans1 = input.countPaths("you", "out")
+        val ans2 = input.countPaths(listOf("svr", "dac", "fft", "out")) +
+            input.countPaths(listOf("svr", "fft", "dac", "out"))
+        return ans1 to ans2
     }
 }
